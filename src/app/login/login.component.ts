f48073962ce5d6869073b006dea54b4a8771c372
unknown <sreeshmaes520@gmail.com>
2023-01-30 10:55:00 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  data = "your perfect banking partner"
+  inputPlaceholder = "account number"
+  acno= ''
+// acn0 : any
+  pass: any
+  userDetails: any = {
+    1000: { acno: 1000, username: "sree", password: "123abc", balance: 0 },
+    1001: { acno: 1001, username: "jithu", password: "abc123", balance: 0 },
+    1002: { acno: 1002, username: "sruthy", password: "1212ab", balance: 0 },
+    1003: { acno: 1003, username: "soorya", password: "ab1213", balance: 0 },
+    1004: { acno: 1004, username: "das", password: "456mno", balance: 0 },
+    1005: { acno: 1005, username: "thazni", password: "123sn", balance: 0 },
+    1006: { acno: 1006, username: "dilbhar", password: "1419ns", balance: 0 },
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  login() 
+  {
+    alert('login clicked')
+   
+  }
+  acnoChange(event:any)
+  {
+       this.acno= event.target.value
+       console.log(this.acno);     
+  }
+  passchange(event:any){
+       this.pass = event.target.value
+       console.log(this.pass);
+       
+  }
+}
+
